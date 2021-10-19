6fb18c5d51309521a8d5d6847be0e1017db73e2c
chshin0225 <chshin0225@gmail.com>
2021-10-19 17:59:31 +0900

    feat: add navigation bar and navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,87 @@
 <template>
   <div id="app">
 
+    <!-- header -->
+    <div class="navbar">
+      <v-img
+        class="logo-img"
+        src="./assets/icon.png"
+        contain
+      >
+      </v-img>
+
+      <v-btn
+       @click="drawer = true"
+       icon
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </div>
+
+    <!-- navigation drawer -->
+    <v-navigation-drawer 
+      v-model="drawer"
+      fixed
+      temporary
+      right
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            {{ username }}님
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in navigationItems"
+          :key="item.title"
+          :to="{ name: item.route }"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      drawer: false,
+      navigationItems: [
+        { title: '홈', icon: 'mdi-home', route: 'Home' },
+        { title: '여행지 추천', icon: 'mdi-airplane', route: 'Home' },
+        { title: '후기 검색', icon: 'mdi-forum-outline', route: 'Home' },
+        { title: 'My 여행', icon: 'mdi-bag-carry-on', route: 'Home' },
+        { title: 'My 자산', icon: 'mdi-cash', route: 'Home' },
+      ],
+      username: '신채원',
+    }
+  },
+}
+</script>
+
 
 <style>
 @import './assets/css/style.css';
@@ -17,5 +95,18 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+</style>
 
+<style scoped>
+.navbar {
+  height: 8vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.logo-img {
+  height: 7vh;
+}
 </style>

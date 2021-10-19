f443e3e6ce4677458b316859d280b2e172e6ba60
Christine Shin <christine@Christines-MacBook-Pro.local>
2021-10-19 16:32:15 +0900

    style: add color palette and font configurations

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#2878A0',
+        secondary: '#FAF8D4',
+        accent: '#BBDDFF',
+        error: '#EF8354',
+        shadow: '#E1E1E1',
+        background: '#F1F4F6',
+      },
+    },
+  },
 });

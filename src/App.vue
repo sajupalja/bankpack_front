4e1a276c4a6e8135efb249573d2a5beea7961e36
chshin0225 <chshin0225@gmail.com>
2021-10-20 16:59:05 +0900

    fix: fix smoke screen bug

--- src/App.vue
+++ src/App.vue
@@ -63,57 +63,11 @@
           <v-list-item-icon>
             <v-icon class="navigation-list-icon">{{ item.icon }}</v-icon>
           </v-list-item-icon>
-
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </router-link>
       </div>
-
-      <!-- <v-list-item
-        v-if="!isLoggedIn"
-        :to="{ name: 'Login' }"
-        link
-      >
-        <v-list-item-content>
-          <v-list-item-title class="nav-drawer-title">
-            로그인하기
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-list-item v-else>
-        <v-list-item-content>
-          <v-list-item-title class="nav-drawer-title">
-            {{ username }}님
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in navigationItems"
-          :key="item.title"
-          :to="{ name: item.route }"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list> -->
     </v-navigation-drawer>
 
     <router-view/>
@@ -171,12 +125,19 @@ export default {
 
 <style scoped>
 .smoke-screen {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 100%;
+  width: 100%;
   z-index: 9999;
+  background-color: white;
 }
 
 .smoke-screen-logo {

e103ccfa4ac54f8d0346954982d640fb3449cebb
chshin0225 <chshin0225@gmail.com>
2021-10-21 09:43:55 +0900

    feat: add new trip button to my trip list page

--- src/views/MyTripList.vue
+++ src/views/MyTripList.vue
@@ -9,7 +9,6 @@
         :key="i"
       >
         <div class="my-trip-card">
-
           <img
             class="my-trip-card-img"
             :src="item.thumbnail"
@@ -22,6 +21,13 @@
         </div>
       </div>
 
+      <!-- 여행 추가 버튼 -->
+      <div class="new-trip-card">
+        <div class="new-trip-card-content">
+          <v-icon class="new-trip-icon">mdi-plus-circle</v-icon>
+          <span>새로운 여행 추가</span>
+        </div>
+      </div>
     </v-container>
   </div>
 </template>
@@ -82,11 +88,22 @@ export default {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 }
 
-.my-trip-card:hover {
+.new-trip-card {
+  height: 132px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  margin: 1rem 0;
+  background-color: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.my-trip-card:hover, .new-trip-card:hover {
   cursor: pointer;
 }
 
-.my-trip-card:active {
+.my-trip-card:active, .new-trip-card:active {
   background-color: var(--background);
 }
 
@@ -111,5 +128,10 @@ export default {
 
 .my-trip-card-date {
   font-size: 0.8rem;
+}
+
+.new-trip-icon {
+  font-size: 2.2rem;
+  margin-right: 0.4rem;
 }
 </style>

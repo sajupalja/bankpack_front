ef10d58b7bd84b66aa1ff6447ec41210e0f0ce7e
chshin0225 <chshin0225@gmail.com>
2021-10-21 14:19:43 +0900

    feat: add tabs to my trip detail page

--- src/views/MyTripDetail.vue
+++ src/views/MyTripDetail.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="my-trip-detail">
-    <h1>이거슨 여행 상세 페이지</h1>
+    <div class="header">
+      <img
+        class="header-img"
+        src="https://lp-cms-production.imgix.net/2021-03/shutterstock_304631102.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850"
+        alt="header-image"
+      >
+    </div>
+    <div class="body">
+      <v-tabs
+        class="tabs"
+        v-model="tab"
+        grow
+      >
+        <v-tabs-slider class="tab-slider"></v-tabs-slider>
+        <v-tab
+          class="tab trip-info-tab"
+          active-class="active-tab"
+        >
+          <router-link
+            class="router-link"
+            :to="{ name: 'MyTripInfo' }"
+          >내 여행</router-link>
+        </v-tab>
+        <v-tab
+          class="tab trip-spending-tab"
+          active-class="active-tab"
+        >
+          <router-link
+            class="router-link"
+            :to="{ name: 'MyTripSpending' }"
+          >지출 관리</router-link>
+        </v-tab>
+      </v-tabs>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -11,5 +45,66 @@ export default {
 </script>
 
 <style scoped>
+.my-trip-detail {
+  background-color: var(--background);
+}
 
+.header {
+  height: 30vh;
+}
+
+.header-img {
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+}
+
+.body {
+  position: relative;
+  top: -50px;
+  height: 1000px;
+  background-color: var(--background);
+  border-top-right-radius: 25px;
+  border-top-left-radius: 25px;
+}
+
+.tabs {
+  border-top-right-radius: 25px;
+  border-top-left-radius: 25px;
+}
+
+.tab-slider {
+  color: var(--primary);
+}
+
+.tab {
+  background-color: var(--background);
+  font-size: 1rem;
+}
+
+.tab:focus {
+  background-color: var(--background);
+  font-size: 1rem;
+}
+
+.trip-info-tab {
+  border-top-left-radius: 25px;
+}
+
+.trip-spending-tab {
+  border-top-right-radius: 25px;
+}
+
+.active-tab {
+  background-color: var(--background);
+  color: var(--primary) !important;
+}
+
+.router-link {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
---
+++ src/views/MyTripSpending.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="my-trip-spending">
+    <v-container>
+
+      <h1>여행 지출</h1>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTripSpending',
+};
+</script>
+
+<style>
+
+</style>

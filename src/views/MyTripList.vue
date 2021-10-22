642bb88a208079c90fbee5660496e96fa5a8bbf1
chshin0225 <chshin0225@gmail.com>
2021-10-22 15:52:02 +0900

    feat: delete new trip button

--- src/views/MyTripList.vue
+++ src/views/MyTripList.vue
@@ -25,14 +25,6 @@
           </div>
         </router-link>
       </div>
-
-      <!-- 여행 추가 버튼 -->
-      <div class="new-trip-card">
-        <div class="new-trip-card-content">
-          <v-icon class="new-trip-icon">mdi-plus-circle</v-icon>
-          <span>새로운 여행 추가</span>
-        </div>
-      </div>
     </v-container>
   </div>
 </template>
@@ -98,22 +90,11 @@ export default {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 }
 
-.new-trip-card {
-  height: 132px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-  margin: 1rem 0;
-  background-color: white;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-}
-
-.my-trip-card:hover, .new-trip-card:hover {
+.my-trip-card:hover {
   cursor: pointer;
 }
 
-.my-trip-card:active, .new-trip-card:active {
+.my-trip-card:active {
   background-color: var(--background);
 }
 
@@ -139,10 +120,5 @@ export default {
 
 .my-trip-card-date {
   font-size: 0.8rem;
-}
-
-.new-trip-icon {
-  font-size: 2.2rem;
-  margin-right: 0.4rem;
 }
 </style>
